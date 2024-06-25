a2a12d8373081549c4ed922079a295168e9dc5a0
Samuel-vilela <146647141+Samuel-vilela@users.noreply.github.com>
2024-06-24 21:01:49 -0300

    Listagem

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const  app = express() 
 
-app.get("/", function(req, res){
-    res.send("Hello world")
-}) 
+const herois = ["homen aranha", "hulk", "ms. marvel"]
 
-app.get("/ptbr", function(req, res){
-    res.send("Olá mundo")
+//ENDPOINT Listagem de todos os herois (READ ALL => [GET])  
+app.get("/heroes", function(req, res){
+    res.send(herois)
+})  
+
+app.get("/heroes/:id", function(req, res){
+    //acessar o parametro de rota ID 
+    const id = req.params.id 
+
+    //pego o item no array
+    const umheroi = herois[id]
+    
+
+    res.send(umheroi)
 })
 
-app.listen(3000)
+app.listen(3000) 
